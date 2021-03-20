20879c17d358b4404a25d44bda3adcc26ab3463c
ragimovalik <ragimovalik@gmail.com>
2021-03-20 13:43:37 +0200

    some cleanup

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'modern-normalize';
 import './index.css';
-// import FirstComponent from './components/FirstComponent';
 import './components/Profile';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <FirstComponent /> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// ReactDOM.render(template, document.getElementById("root"));
