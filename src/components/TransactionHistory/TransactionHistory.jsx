039306ed49450332a66b37327348cd4ec8ebbc63
ragimovalik <ragimovalik@gmail.com>
2021-03-22 20:44:56 +0200

    In-progress - Task 4 added

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,53 +1,43 @@
+import PropTypes from 'prop-types';
 import styles from './TransactionHistory.module.css';
 
-console.log(styles);
+const TransactionHistory = ({ items }) => {
+  const tableBody = items.map(({ amount, currency, id, type }, idx) => {
+    const aaa = idx % 2 === 0 ? '#ffffff' : '#d1ebff';
+    return (
+      <tr key={id} style={{ backgroundColor: aaa }}>
+        <td>{type[0].toUpperCase() + type.substring(1)}</td>
+        <td>{amount}</td>
+        <td>{currency}</td>
+      </tr>
+    );
+  });
 
-function TransactionHistory(items) {
-  return <div>kdjhakjfh</div>;
-}
+  return (
+    <table className={styles.Transactions}>
+      <thead className={styles.Transactions__head}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>{tableBody}</tbody>
+    </table>
+  );
+};
+
+// TransactionHistory.defaultProps = {}
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+    }),
+  ),
+};
 
 export default TransactionHistory;
-
-/*
-<table class="transaction-history">
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
-
-  <tbody>
-    <tr>
-      <td>Invoice</td>
-      <td>125</td>
-      <td>USD</td>
-    </tr>
-    <tr>
-      <td>Withdrawal</td>
-      <td>85</td>
-      <td>USD</td>
-    </tr>
-  </tbody>
-</table>
-
-
-Данные для списка доступны в формате JSON 
-в файле transactions.json. Это массив объектов, 
-каждый объект описывает одну транзакцию со 
-следующими свойствами:
-
-id — уникальный идентификатор транзакции
-type — тип транзакции
-amount - сумма транзакции
-currency - тип валюты
-Описание компонента
-Необходимо создать компонент <TransactionHistory>
-принимающий один проп items - массив объектов 
-транзакций из transactions.json. Компонент создает 
-разметку таблицы. Каждая транзакция это строка таблицы. 
-В примере приведена разметка двух транзакций.
-
-
-*/
