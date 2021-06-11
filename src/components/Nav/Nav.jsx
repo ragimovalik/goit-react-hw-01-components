722c56ef8c9c2283a9780d517b0700fd80a0963d
ragimovalik <ragimovalik@gmail.com>
2021-06-11 20:45:05 +0300

    Clean Readme

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import { createUseStyles } from 'react-jss';
-import { routes } from '../../routes';
+// import { routes } from '../../routes';
 
 const useStyles = createUseStyles({
   Flex: {
